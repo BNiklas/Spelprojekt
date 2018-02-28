cd32652ca9e3e04b4df87ceec8efa18a51a7cc3f
Tobias Hamerslag <hamerslag.tobias@gmail.com>
2018-02-28 16:30:06 +0100

    Add StateManager

--- src/CombatState.java
+++ src/CombatState.java
@@ -1,2 +1,34 @@
-public class CombatState {
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.terminal.Terminal;
+
+public class CombatState extends State {
+    @Override
+    public void onInit() {
+
+    }
+
+    @Override
+    public void onInput(Key key) {
+
+    }
+
+    @Override
+    public void onLoop() {
+
+    }
+
+    @Override
+    public void onDraw(Terminal terminal) {
+
+    }
+
+    @Override
+    public void enter() {
+
+    }
+
+    @Override
+    public void exit() {
+
+    }
 }
