75f1826cedffe8f3216a6770ab2c61166dc413ce
Tobias Hamerslag <hamerslag.tobias@gmail.com>
2018-02-28 15:09:55 +0100

    Add movement, fix graphic

--- src/Player.java
+++ src/Player.java
@@ -2,4 +2,8 @@ public class Player extends Character {
     public Player(int x, int y, char look) {
         super(x, y, look);
     }
+
+    public void onMove(int changeX, int changeY) {
+        move(changeX, changeY);
+    }
 }
