3381355c555e25d5ff32e361599c3228a4e9d8ec
Simon Pellijeff <simonpellijeff@gmail.com>
2018-03-01 14:32:38 +0100

    update walls in room

--- src/GameObject/Hill.java
+++ src/GameObject/Hill.java
@@ -1,7 +1,30 @@
 package GameObject;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Hill extends Obstacle{
     public Hill(int x, int y) {
         super(x, y, '\u0394', false);
     }
+
+
+   /* public static List<Hill> getHills()
+
+
+
+
+
+    public static List<Monster> getRandomAmountOfMonsters(int bound){
+        ArrayList<Monster> monsters = new ArrayList<>();
+        int padding = 10;
+        int screenWidth = 70;
+        int screenHeight = 30;
+        Random rand = new Random();
+        for(int i = rand.nextInt(bound); i < bound; i++){
+            monsters.add(new Monster(getRandomNumberInRange(padding, (screenWidth - padding)),
+                    getRandomNumberInRange(padding, (screenHeight - padding)), 'M'));
+        }
+        return monsters;*/
 }
--- src/Room/Room.java
+++ src/Room/Room.java
@@ -82,10 +82,10 @@ public class Room {
     private void addWalls(List<GameObject> gameObjects){
         List<Wall> walls = new ArrayList<>();
         for(int x = 0; x <= SCREEN_WIDTH; x++){
-            walls.add(new Wall(x, SCREEN_HEIGHT));
+            walls.add(new Wall(x, SCREEN_HEIGHT - 1));
             walls.add(new Wall(x, 0));
         }
-        for(int y = 0; y <= SCREEN_HEIGHT; y++){
+        for(int y = 0; y < SCREEN_HEIGHT; y++){
             walls.add(new Wall(SCREEN_WIDTH, y));
             walls.add(new Wall(0, y));
         }
