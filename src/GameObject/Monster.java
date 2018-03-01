01193475e25acc4e9cb18846300b15e0f9c46508
Simon Pellijeff <simonpellijeff@gmail.com>
2018-03-01 11:30:06 +0100

    Add functionality to add items and monsters to rooms

--- src/GameObject/Monster.java
+++ src/GameObject/Monster.java
@@ -1,5 +1,9 @@
 package GameObject;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Monster extends Character {
     public Monster(int x, int y, char look) {
         super(x, y, look);
@@ -31,6 +35,26 @@ public class Monster extends Character {
         } else if(this.getY() > object.getY()){
             move(0, -this.getSpeed());
         }
+    }
+    public static List<Monster> getRandomAmountOfMonsters(int bound){
+        ArrayList<Monster> monsters = new ArrayList<>();
+        int padding = 10;
+        int screenWidth = 70;
+        int screenHeight = 30;
+        Random rand = new Random();
+        for(int i = rand.nextInt(bound); i < bound; i++){
+            monsters.add(new Monster(getRandomNumberInRange(padding, (screenWidth - padding)),
+                    getRandomNumberInRange(padding, (screenHeight - padding)), 'M'));
+        }
+        return monsters;
+    }
+    private static int getRandomNumberInRange(int min, int max){
+        Random rand = new Random();
+        int randomNum = Integer.MIN_VALUE;
+        while(randomNum < min){
+            randomNum = rand.nextInt(max);
+        }
+        return randomNum;
     }
 
 }
--- src/Room/Room.java
+++ src/Room/Room.java
@@ -1,6 +1,7 @@
 package Room;
 
 import GameObject.*;
+import Item.Valuable;
 import com.googlecode.lanterna.terminal.Terminal;
 
 import java.util.ArrayList;
@@ -9,9 +10,23 @@ import java.util.List;
 public class Room {
     private List<GameObject> gameObjects;
 
+    private static final int SCREENWIDTH = 70;
+    private static final int SCREENHEIGHT = 30;
+
+
     public Room(Player player) {
         this.gameObjects = new ArrayList<>();
         gameObjects.add(player);
+    }
+    public Room(Player player, List<GameObject> gameObjects){
+
+    }
+    public static int getScreenwidth() {
+        return SCREENWIDTH;
+    }
+
+    public static int getScreenheight() {
+        return SCREENHEIGHT;
     }
 
     public void onInit() {
@@ -28,5 +43,11 @@ public class Room {
         for (GameObject gameObject : gameObjects) {
             gameObject.onDraw(terminal);
         }
+    }
+    private void addRandomItems(List<GameObject> gameObjects){
+        gameObjects.addAll(Valuable.getRandomValuables(3));
+    }
+    private void addMonsters(List<GameObject> gameObjects){
+        gameObjects.addAll(Monster.getRandomAmountOfMonsters(4));
     }
 }
