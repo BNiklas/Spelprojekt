9e84855e466297e59406eddb45b34d98d635f8ba
Simon Pellijeff <simonpellijeff@gmail.com>
2018-03-02 10:05:11 +0100

    Add healthbar to combat state

--- src/StateManager/CombatState.java
+++ src/StateManager/CombatState.java
@@ -16,15 +16,15 @@ public class CombatState extends State {
     boolean clear = false;
 
     final int[][] dmgArray = {{0, 1, 1, 1, 1, 1, 1, 2, 2, 2},
-                              {1, 0, 1, 1, 2, 2, 3, 3, 4, 5},
-                              {1, 1, 0, 1, 1, 2, 2, 3, 3, 4},
-                              {1, 1, 1, 0, 1, 1, 2, 2, 3, 3},
-                              {1, 2, 1, 1, 0, 1, 1, 2, 2, 3},
-                              {1, 2, 2, 1, 1, 0, 1, 1, 2, 2},
-                              {1, 3, 2, 2, 1, 1, 0, 1, 1, 2},
-                              {2, 3, 3, 2, 2, 1, 1, 0, 1, 1},
-                              {2, 4, 3, 3, 2, 2, 1, 1, 0, 1},
-                              {2, 5, 4, 3, 3, 2, 2, 1, 1, 0}};
+            {1, 0, 1, 1, 2, 2, 3, 3, 4, 5},
+            {1, 1, 0, 1, 1, 2, 2, 3, 3, 4},
+            {1, 1, 1, 0, 1, 1, 2, 2, 3, 3},
+            {1, 2, 1, 1, 0, 1, 1, 2, 2, 3},
+            {1, 2, 2, 1, 1, 0, 1, 1, 2, 2},
+            {1, 3, 2, 2, 1, 1, 0, 1, 1, 2},
+            {2, 3, 3, 2, 2, 1, 1, 0, 1, 1},
+            {2, 4, 3, 3, 2, 2, 1, 1, 0, 1},
+            {2, 5, 4, 3, 3, 2, 2, 1, 1, 0}};
 
     public void setFighters() {
         this.player = new Player(1, 1, 'a');
@@ -40,16 +40,16 @@ public class CombatState extends State {
     public void onInput(Key key) {
         switch (key.getKind()) {
             case ArrowUp:
-                if (playerAttack<player.getStamina())
+                if (playerAttack < player.getStamina())
                     playerAttack++;
                 break;
             case ArrowDown:
-                if(playerAttack>0)
+                if (playerAttack > 0)
                     playerAttack--;
                 break;
             case Enter:
                 Random rand = new Random();
-                enemyAttack=rand.nextInt(10);
+                enemyAttack = rand.nextInt(10);
                 updateHealth();
                 updateStamina();
                 break;
@@ -64,35 +64,34 @@ public class CombatState extends State {
 
     @Override
     public void onDraw(Terminal terminal) {
-        if(enemy.getHealth()>0 && player.getHealth()>0) {
+        if (enemy.getHealth() > 0 && player.getHealth() > 0) {
             printCombatText(terminal);
-        }
-        else if(!clear) {
+        } else if (!clear) {
             terminal.clearScreen();
-            clear=true;
+            clear = true;
         }
-        if (enemy.getHealth()<1)
+        if (enemy.getHealth() < 1)
             Graphic.printString(terminal, "You killed the " + enemy.getType(), 32, 13);
-        else if(player.getHealth()<1)
-            Graphic.printString(terminal, "The "+enemy.getType()+" killed you!", 32, 13, Terminal.Color.RED);
+        else if (player.getHealth() < 1)
+            Graphic.printString(terminal, "The " + enemy.getType() + " killed you!", 32, 13, Terminal.Color.RED);
     }
 
 
     private void updateHealth() {
-        if(playerAttack > enemyAttack)
-            enemy.setHealth(enemy.getHealth()-(Math.abs(dmgArray[playerAttack][enemyAttack])+player.getBestWeapon().getAttackValue()));
-        else if(enemyAttack > playerAttack)
-            player.setHealth(player.getHealth()-Math.abs(dmgArray[enemyAttack][playerAttack]));
+        if (playerAttack > enemyAttack)
+            enemy.setHealth(enemy.getHealth() - (Math.abs(dmgArray[playerAttack][enemyAttack]) + player.getBestWeapon().getAttackValue()));
+        else if (enemyAttack > playerAttack)
+            player.setHealth(player.getHealth() - Math.abs(dmgArray[enemyAttack][playerAttack]));
     }
 
     private void updateStamina() {
-        if(playerAttack==0 && !(player.getStamina()+3>9))
-            player.setStamina(player.getStamina()+3);
-        else if(playerAttack==0 && !(player.getStamina()+1>9))
-            player.setStamina(player.getStamina()+(9-player.getStamina()));
+        if (playerAttack == 0 && !(player.getStamina() + 3 > 9))
+            player.setStamina(player.getStamina() + 3);
+        else if (playerAttack == 0 && !(player.getStamina() + 1 > 9))
+            player.setStamina(player.getStamina() + (9 - player.getStamina()));
         else
-            player.setStamina(player.getStamina()-(int)Math.ceil((double)playerAttack/2));
-        playerAttack=player.getStamina();
+            player.setStamina(player.getStamina() - (int) Math.ceil((double) playerAttack / 2));
+        playerAttack = player.getStamina();
     }
 
     @Override
@@ -104,6 +103,7 @@ public class CombatState extends State {
     public void exit() {
 
     }
+
     private void printCombatText(Terminal terminal) {
         //Tutorial
         Graphic.printString(terminal, "HOW TO PLAY:", 72, 1);
@@ -121,8 +121,8 @@ public class CombatState extends State {
 
         //Extra wall
         terminal.applyForegroundColor(Terminal.Color.CYAN);
-        for(int i=70; i<100; i++){
-            terminal.moveCursor(i,23);
+        for (int i = 70; i < 100; i++) {
+            terminal.moveCursor(i, 23);
             terminal.putCharacter('\u2588');
         }
         terminal.applyForegroundColor(Terminal.Color.WHITE);
@@ -130,21 +130,23 @@ public class CombatState extends State {
         //Enemy stats
         Graphic.printString(terminal, "You have encountered a " + enemy.getType(), 4, 2);
         Graphic.printString(terminal, "-Stamina: " + enemy.getStamina(), 6, 3);
-        Graphic.printString(terminal, "-Health: " + enemy.getHealth()+" ", 6, 4);
+        Graphic.printString(terminal, "-Health: " + enemy.getHealth() + " ", 6, 4);
         //Combat texts
-        if(playerAttack==0)
+        if (playerAttack == 0)
             Graphic.printString(terminal, "You will Defend this turn!", 27, 13);
         else
             Graphic.printString(terminal, "How hard will you hit?:  " + playerAttack, 27, 13);
-        if(enemyAttack!=-1) {
+        if (enemyAttack != -1) {
             Graphic.printString(terminal, "Enemy hits for... " + enemyAttack, 27, 14);
             Graphic.printString(terminal, "Enemy hits for... " + enemyAttack, 27, 14);
         }
         //Player stats
         Graphic.printString(terminal, "Player status ", 47, 25);
         Graphic.printString(terminal, "-Stamina: " + player.getStamina(), 50, 26);
-        Graphic.printString(terminal, "-Health: " + player.getHealth()+" ", 50, 27);
-        Graphic.printString(terminal, "Your weapon:", 75, 26);
-        Graphic.printString(terminal, player.getBestWeapon().toString()+"to damage", 75, 27);
+        Graphic.printString(terminal, "-Health: " + player.getHealth() + " ", 50, 27);
+        Graphic.printString(terminal, "Health: " + player.getHealth(), 75, 24);
+        Graphic.printHealthbar(terminal, 75, 25, player.getHealth());
+        Graphic.printString(terminal, "Your weapon:", 75, 27);
+        Graphic.printString(terminal, player.getBestWeapon().toString() + "to damage", 75, 28);
     }
 }
