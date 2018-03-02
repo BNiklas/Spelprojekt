50a03210ec58fdc9667dedf705932a6a231b6c22
Maria Sjölin <mia.sjolin@gmail.com>
2018-03-02 11:52:13 +0100

    remove unused constructor

--- src/GameObject/Obstacles/Obstacle.java
+++ src/GameObject/Obstacles/Obstacle.java
@@ -4,9 +4,6 @@ import GameObject.GameObject;
 import com.googlecode.lanterna.terminal.Terminal;
 
 public class Obstacle extends GameObject{
-    public Obstacle(int x, int y, char look, boolean traversable) {
-        super(x, y, look, traversable);
-    }
 
     public Obstacle(int x, int y, char look, Terminal.Color color, boolean traversable) {
         super(x, y, look, color, traversable);
